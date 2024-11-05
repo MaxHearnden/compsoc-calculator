db59f4507db52a8133d0a27084d3c0f0cf751872
leo2006-206 <82983076+leo2006-206@users.noreply.github.com>
2024-11-05 18:52:31 +0000

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -49,7 +49,7 @@ class Calculator:
             "0", "1", "2", "3",
             "4", "5", "6", "7",
             "8", "9", "+", "-",
-            "*", "/", "%", "=" ]):
+            "*", "/", "++++", "=" ]):
             button = self.makeButton(button_name)
             button.button.grid(column = index % COLUMNS, row = index // COLUMNS + 1)
         print("Compsoc calculator")
